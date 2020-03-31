72721e66fa06d0327a5b7d29f90c6081c145f587
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-03-31 17:06:43 +0500

    2020

--- Lab manual week 1/Task2.java
+++ Lab manual week 1/Task2.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 class Task2
 {
-public static void main(String args[])
-{
-Scanner TakeScan = new Scanner(System.in);
+    public static void main(String args[])
+    {
+        Scanner TakeScan = new Scanner(System.in);
 
-System.out.println("Entered Letter: ");
-char Letter1 = TakeScan.next().charAt(0);
-char Letter2 = 'A';
-System.out.println("You Entered Letter 1: "+Letter1);
-System.out.println("Your Fixed Letter 2: " +Letter2);
-}
+        System.out.println("Entered Letter: ");
+        char Letter1 = TakeScan.next().charAt(0);
+        char Letter2 = 'A';
+        System.out.println("You Entered Letter 1: "+Letter1);
+        System.out.println("Your Fixed Letter 2: " +Letter2);
+    }
 }
