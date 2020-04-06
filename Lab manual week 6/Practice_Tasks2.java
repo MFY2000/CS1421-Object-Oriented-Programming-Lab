f894e9a2a16caecdb339f04e07f4102df422ce14
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-04-06 10:45:29 +0500

    4/6/2020

--- Lab manual week 6/Practice_Tasks2.java
+++ Lab manual week 6/Practice_Tasks2.java
@@ -1,15 +1,45 @@
+import java.util.Scanner;
 
-/**
- *  Multiple Choice Question
- */
-public class  MultipleChoiceQuestion {
+public class Practice_Tasks2
+{
+   private String text;
+   private String answer;
+  
 
-
-    
-    
-}
-
-
-public class Practice_Tasks2 {
+   public Question()
+   {
+       text="";
+       answer="";
+      
+   }
+   public static void presentQuestion(Question q)
+   {
+       q.display();
+       System.out.print("Your answer: ");
+       Scanner scan = new Scanner(System.in);
+       String response =scan.nextLine();
+       System.out.println(q.checkAnswer(response));
+   }
+  
+   public void setText(String questionText)
+   {
+       text=questionText;
+   }
+  
+   public void setAnswer(String correctResponse)
+   {
+       answer=correctResponse;
+   }
+  
+   public boolean checkAnswer(String response)
+   {
+      
+       return response.replaceAll("\\s+","").toLowerCase().equals(answer.replaceAll("\\s+","").toLowerCase());
+   }
+  
+   public void display()
+   {
+       System.out.println(text);
+   }
 
 }
