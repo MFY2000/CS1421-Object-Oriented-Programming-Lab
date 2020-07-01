d5d25a68de9a3108cacb507a316fd945b4f1482a
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-07-01 14:51:19 +0500

    7/1/2020

---
+++ Lab Final Exam/src/com/company/Question1.java
@@ -0,0 +1,32 @@
+package com.company;
+
+class Car{
+
+    public void getTrimLevel(){
+
+    }
+}
+
+class Engine{
+    public void accelerate(){
+
+    }
+    public void getFuelLevel(){
+
+    }
+}
+
+
+
+class test{
+    public static void main(String[] args) {
+
+    }
+}
+
+
+public class Question1 {
+    public static void main(String[] args) {
+        test.main(args);
+    }
+}
